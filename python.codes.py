b918a07dbd3fdbc0b80ebb204c087bd2b8796f4f
Avinash-Mandha <avinashmandha0521@gmail.com>
2026-06-11 12:18:20 +0530

    Add files via upload

---
+++ python.codes.py
@@ -0,0 +1,15 @@
+# import turtle
+# t =turtle.Turtle()
+#     #printing triangle
+# for i in range(3):
+#     t.forward(100)
+#     t.left(120);
+# import turtle 
+# t = turtle.Turtle()
+# t.circle (50)
+# t.color  ("blue")
+import turtle 
+t =turtle.Turtle()
+for i in range (5):
+     t.forward (100)
+     t.right(144)
